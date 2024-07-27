e6caab1abda9770663d3af3cc9d864027cb3c6f3
HetP1742431 <hetbhar@ualberta.ca>
2024-07-27 02:50:56 -0600

    Configured backend

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,50 @@
 import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import http from "http";
+import { Server } from "socket.io";
 import dotenv from "dotenv";
-import connectDB from "./utils/db.js";
 
-dotenv.config({});
+// Load environment variables
+dotenv.config();
+
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+const options = {
+  origin: "http://localhost:5173",
+};
 
 // Middleware
+app.use(cors(options));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Connect to MongoDB
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB connected successfully");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+//Socket.IO configuration
+io.on("connection", (socket) => {
+  console.log("User connected");
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+});
+
+// Testing home path for server
 app.get("/", (req, res) => {
   return res.send("Hello from server");
 });
 
+// Connect and listen to PORT
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   connectDB();
