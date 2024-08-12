09a27096ba5b2e503f0a27e759612221ccc46506
HetP1742431 <hetbhar@ualberta.ca>
2024-08-12 02:47:02 -0600

    Update baseURL for api

--- frontend/src/apiConfig.js
+++ frontend/src/apiConfig.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-export const baseURL = "http://localhost:8000";
+export const baseURL =
+  "https://collaborative-whiteboard-54thnvly5-hetp1742431s-projects.vercel.app";
 
 export const api = axios.create({
   baseURL: baseURL,
